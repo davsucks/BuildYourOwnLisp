5ca04fa46ed3d44088d7106d703ad40fcc42e994
David Suckstorff <dsucksto@thoughtworks.com>
2020-02-29 15:03:41 -0600

    Rename prompt to parsing and add mpc stuff

--- my-src/parsing.c
+++ my-src/parsing.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "mpc.h"
 
 #ifdef _WIN32
 #include <string.h>
@@ -26,22 +25,6 @@ void add_history(char* unused) {}
 
 int main(int argc, char** argv) {
 
-    /* Create some parsers */
-    mpc_parser_t* Number = mpc_new("number");
-    mpc_parser_t* Operator = mpc_new("operator");
-    mpc_parser_t* Expr = mpc_new("expr");
-    mpc_parser_t* Lispy = mpc_new("lispy");
-
-    /* Define them with the following Language */
-    mpca_lang(MPCA_LANG_DEFAULT,
-            "                                                   \
-                number: /-?[0-9]+/ ;                            \
-                operator: '+' | '-' | '*' | '/' ;               \
-                expr: <number> | '(' <operator> <expr>+ ')' ;   \
-                lispy: /^/ <operator> <expr>+ /$/ ;             \
-            ",
-            Number, Operator, Expr, Lispy);
-
     /* Print Versiona and Exit information */
     puts("Lispy Version 0.0.0.0.1");
     puts("Press Ctrl+c to Exit\n");
@@ -60,9 +43,6 @@ int main(int argc, char** argv) {
         /* Free retrieved input */
         free(input);
     }
-
-    /* Undefine and Delete our Parsers */
-    mpc_cleanup(4, Number, Operator, Expr, Lispy);
 
     return 0;
 }
